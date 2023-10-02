a6c50f6444c67598c7454ef0b6a79ea03080d297
6Seraph <130529825+6Seraph@users.noreply.github.com>
2023-10-02 01:13:25 +0000

    refactoring test for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,67 +3,56 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 
-test("adds task", () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  )
-  const input = screen.getByPlaceholderText("What need to be done?")
-  fireEvent.change(input, { target: { value: "new_task_1" } })
-  fireEvent.keyUp(input, { key: "Enter" })
-  fireEvent.change(input, { target: { value: "new_task_2" } })
-  fireEvent.keyUp(input, { key: "Enter" })
-  fireEvent.change(input, { target: { value: "new_task_3" } })
-  fireEvent.keyUp(input, { key: "Enter" })
-  expect(screen.getByText("3 items left")).toBeInTheDocument()
-  expect(screen.getByText("new_task_1")).toBeInTheDocument()
-  expect(screen.getByText("new_task_2")).toBeInTheDocument()
-  expect(screen.getByText("new_task_3")).toBeInTheDocument()
-})
+describe("App works", () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    )
+  })
+  test("adds tasks", () => {
+    const input = screen.getByPlaceholderText("What need to be done?")
+    for (let i = 1; i <= 3; i++) {
+      const name = "new_task_" + i
+      fireEvent.change(input, { target: { value: name } })
+      fireEvent.keyUp(input, { key: "Enter" })
+      expect(screen.getByText(name)).toBeInTheDocument()
+    }
+    expect(screen.getByText("3 items left")).toBeInTheDocument()
+  })
 
-test("toggles task status", () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  )
-  const task = screen.getByText("new_task_1")
-  fireEvent.click(task)
-  expect(task).toHaveClass("text-decoration-line-through text-black-50")
-  fireEvent.click(task)
-  expect(task).not.toHaveClass("text-decoration-line-through text-black-50")
-  fireEvent.click(task)
-})
+  test("toggles task status", () => {
+    const task = screen.getByText("new_task_1")
+    const click = (): void => {
+      fireEvent.click(task)
+    }
+    click()
+    expect(task).toHaveClass("text-decoration-line-through text-black-50")
+    click()
+    expect(task).not.toHaveClass("text-decoration-line-through text-black-50")
+    click()
+  })
 
-test("removes task", () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  )
-  const task = screen.getByText("new_task_3")
-  const delButton = task.nextElementSibling
-  fireEvent.click(delButton!)
-  expect(task).not.toBeInTheDocument()
-})
+  test("removes task", () => {
+    const task = screen.getByText("new_task_3")
+    const delButton = task.nextElementSibling
+    fireEvent.click(delButton!)
+    expect(task).not.toBeInTheDocument()
+  })
 
-test("filters tasks", () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  )
-  const filterActive = screen.getByRole("radio", { name: "active" })
-  fireEvent.click(filterActive)
-  expect(screen.queryByText("new_task_1")).not.toBeInTheDocument()
-  expect(screen.getByText("new_task_2")).toBeInTheDocument()
-  const filterCompleted = screen.getByRole("radio", { name: "completed" })
-  fireEvent.click(filterCompleted)
-  expect(screen.getByText("new_task_1")).toBeInTheDocument()
-  expect(screen.queryByText("new_task_2")).not.toBeInTheDocument()
-  const filterAll = screen.getByRole("radio", { name: "all" })
-  fireEvent.click(filterAll)
-  expect(screen.getByText("new_task_1")).toBeInTheDocument()
-  expect(screen.getByText("new_task_2")).toBeInTheDocument()
+  test("filters tasks", () => {
+    const filterActive = screen.getByRole("radio", { name: "active" })
+    fireEvent.click(filterActive)
+    expect(screen.queryByText("new_task_1")).not.toBeInTheDocument()
+    expect(screen.getByText("new_task_2")).toBeInTheDocument()
+    const filterCompleted = screen.getByRole("radio", { name: "completed" })
+    fireEvent.click(filterCompleted)
+    expect(screen.getByText("new_task_1")).toBeInTheDocument()
+    expect(screen.queryByText("new_task_2")).not.toBeInTheDocument()
+    const filterAll = screen.getByRole("radio", { name: "all" })
+    fireEvent.click(filterAll)
+    expect(screen.getByText("new_task_1")).toBeInTheDocument()
+    expect(screen.getByText("new_task_2")).toBeInTheDocument()
+  })
 })
