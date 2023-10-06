aeba46fea6cf0ecaa6adfe8f5e50cb889502bc9f
6Seraph <130529825+6Seraph@users.noreply.github.com>
2023-10-06 03:18:39 +0000

    Optimize select from store with createSelector

--- src/features/todoList/components/List.tsx
+++ src/features/todoList/components/List.tsx
@@ -1,3 +1,4 @@
+import { createSelector } from "@reduxjs/toolkit"
 import { Todo, selectTodoList, selectTodoListFilter } from ".."
 import { useAppSelector } from "../../../app/hooks"
 import { Item } from "."
@@ -6,23 +7,24 @@ function createItems(list: Todo[]) {
   return list.map((todo) => <Item key={todo.id}>{todo}</Item>)
 }
 
+const selectTodosByFilter = createSelector(
+  [selectTodoList, selectTodoListFilter],
+  (todos, filter) => {
+    switch (filter) {
+      case "all":
+        return createItems(todos)
+      case "active":
+        return createItems(todos.filter((todo) => todo.active))
+      case "completed":
+        return createItems(todos.filter((todo) => !todo.active))
+    }
+  },
+)
+
 function List() {
-  const list = useAppSelector(selectTodoList)
-  const filter = useAppSelector(selectTodoListFilter)
+  const list = useAppSelector(selectTodosByFilter)
 
-  const filteredList: JSX.Element[] = []
-  if (filter === "active") {
-    filteredList.push(...createItems(list.filter((todo) => todo.active)))
-  }
-  if (filter === "completed") {
-    filteredList.push(...createItems(list.filter((todo) => !todo.active)))
-  }
-
-  return (
-    <ul className="list-group row">
-      {filter === "all" ? createItems(list) : filteredList}
-    </ul>
-  )
+  return <ul className="list-group row">{list}</ul>
 }
 
 export default List
