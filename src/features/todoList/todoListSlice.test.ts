40383a01b752605cea6cd3c6a0eaaae3fdadcc77
6Seraph <130529825+6Seraph@users.noreply.github.com>
2023-10-06 02:59:57 +0000

    add todoListSlice test file

---
+++ src/features/todoList/todoListSlice.test.ts
@@ -0,0 +1,53 @@
+import reducer, {
+  add,
+  remove,
+  changeState,
+  clear,
+  toggleFilter,
+} from "./todoListSlice"
+
+describe("Store works", () => {
+  const todo1 = { active: true, id: "1", text: "foo" }
+  const todo2 = { active: true, id: "2", text: "bar" }
+  const todo3 = { active: true, id: "3", text: "foobar" }
+
+  test("adds todo to store", () => {
+    const result = reducer({ list: [], filter: "all" }, add(todo1))
+    expect(result.list[0]).toEqual(todo1)
+  })
+
+  test("removs todo from store", () => {
+    let result = reducer({ list: [], filter: "all" }, add(todo1))
+    result = reducer(result, add(todo2))
+    result = reducer(result, add(todo3))
+    result = reducer(result, remove("2"))
+    expect(result.list[0]).toEqual(todo1)
+    expect(result.list[1]).toEqual(todo3)
+    expect(result.list[2]).toBeUndefined()
+  })
+
+  test("changes todo status", () => {
+    let result = reducer({ list: [], filter: "all" }, add(todo1))
+    result = result = reducer(result, changeState("1"))
+    expect(result.list[0].active).toBe(false)
+  })
+
+  test("clears list from todos", () => {
+    let result = reducer({ list: [], filter: "all" }, add(todo1))
+    result = reducer(result, add(todo2))
+    result = reducer(result, add(todo3))
+    result = reducer(result, changeState("1"))
+    result = reducer(result, changeState("3"))
+    result = reducer(result, clear())
+    expect(result.list[0]).toEqual(todo2)
+  })
+
+  test("toggles filter", () => {
+    let result = reducer({ list: [], filter: "all" }, toggleFilter("active"))
+    expect(result.filter).toBe("active")
+    result = reducer(result, toggleFilter("all"))
+    expect(result.filter).toBe("all")
+    result = reducer(result, toggleFilter("completed"))
+    expect(result.filter).toBe("completed")
+  })
+})
